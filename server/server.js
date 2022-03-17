ab0d18950f2a47cdf18da9d83dbb0e018f26bbb1
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2022-03-17 18:22:30 -0400

    WIP

--- server/server.js
+++ server/server.js
@@ -1,39 +1,21 @@
-const jwt = require('jsonwebtoken');
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
 
-// set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization || req.body.token;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-    if (!token) {
-      return req;
-    }
+app.use(routes);
 
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    return req;
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-};
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
